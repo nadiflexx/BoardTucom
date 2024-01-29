e0573adf3685a4b2ca5542b7f376a35fa3888d1f
Nadiflexx <nadi-1999@hotmail.es>
2024-01-29 22:04:55 +0100

    #1 FINAL VERSION

---
+++ src/main/java/com/dam2t/dao/JuegoCartasDao.java
@@ -0,0 +1,28 @@
+package com.dam2t.dao;
+
+import java.util.ArrayList;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.dam2t.model.JuegoCartas;
+
+/**
+ * Represents a Card Game interface to manipulate database data (CRUD) with SQL
+ * functions integrated in JpaRepository.
+ * 
+ * @author Nadeem Rashid Arias
+ * @version 1.0
+ * @since 1.0
+ */
+public interface JuegoCartasDao extends JpaRepository<JuegoCartas, Integer> {
+	/**
+	 * Selects Card Games that have the same season.
+	 * 
+	 * @param temporada Integer representing the season to search.
+	 * @return an ArrayList of all the Card Games with the same season.
+	 */
+	@Query(value = "SELECT * FROM juego_cartas WHERE temporada=:temporada", nativeQuery = true)
+	ArrayList<JuegoCartas> findJuegoCartasByTemporada(@Param("temporada") int temporada);
+}
---
+++ src/test/java/com/dam2t/BoardTucomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dam2t;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardTucomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
